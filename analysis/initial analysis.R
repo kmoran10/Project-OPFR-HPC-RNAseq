43f5b96f656e2ddd1eb78a215f92e0b3021cd83b
kmoran10 <kevinmoran1210@gmail.com>
2024-10-24 13:37:00 -0400

    initial 2

--- analysis/initial analysis.R
+++ analysis/initial analysis.R
@@ -12,6 +12,8 @@ library(pheatmap)
 library(annotables)
 library(ggpubr)
 
+source("functions/geom_boxjitter.R")
+
 
 rawcounts <- read.csv("rawdata/KT_Counts.csv")
 bcx1 <- rawcounts %>% column_to_rownames(.,var = "X")
@@ -33,6 +35,10 @@ colSums(bcx1[,]) %>%
 genecounts <- genecounts %>%
   full_join(id) 
 
+
+#########
+
+
 genecounts %>% 
   ggplot(aes(genecounts)) +
   geom_histogram(bins = 54,alpha =0.5,color = 'grey') +
@@ -49,8 +55,6 @@ ggsave("imgs/braincounts_byid.png",counts, height = 5,width= 5, dpi = 150)
 
 
 ####
-
-source("functions/geom_boxjitter.R")
 
 counts2 <- genecounts %>%
   ggplot(aes(treatment,genecounts, color = treatment))+
@@ -136,4 +140,27 @@ summary(lm1)
 
 
 # what about E-level?
+genecounts %>%
+  ggplot(aes(treatment,genecounts, fill = E_level))+
+  geom_boxjitter(outlier.color = NA, jitter.shape = 21, 
+                 alpha = 1,
+                 width = 0.5,
+                 jitter.height = 0.02, jitter.width = 0.02, errorbar.draw = TRUE,
+                 position = position_dodge(0.8)) +
+  scale_fill_manual(values=c("hotpink", "pink", "skyblue")) +
+  labs(title="Counts~Treatment*E_Level",x="Group", y = "Gene Counts") +
+  theme_classic() +
+  theme(axis.line = element_line(colour = 'black', size = 1),
+        axis.ticks = element_line(colour = "black", size = 1),
+        #        legend.position="none",
+        axis.text=element_text(size=16),
+        axis.title=element_text(size=18,face="bold"),
+        plot.title = element_text(size=24, hjust = 0.4)) 
 
+lm2 <- lm(genecounts~treatment*E_level, data = genecounts)
+summary(lm2)
+
+lm3 <- lm(genecounts~treatment+E_level, data = genecounts)
+summary(lm3)
+
+# while it may look it somewhat, no effect or interaction of sex or e level on raw count
---
+++ analysis/limma.R
@@ -0,0 +1,16 @@
+
+# limma
+
+library(limma)
+library(Glimma)
+library(edgeR)
+
+library(biomaRt)
+library(AnnotationDbi)
+library(annotables)
+grcm38 <- grcm38
+library(tidyverse)
+
+
+
+
